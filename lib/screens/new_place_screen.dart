1316fc26de480cf3de1451f8b1ba8233f7415e22
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-03-02 14:40:58 +0700

    Getting the User's Current Location

---
+++ lib/screens/new_place_screen.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favorite_places/providers/user_places.dart';
+
+class NewPlaceScreen extends ConsumerStatefulWidget {
+  const NewPlaceScreen({super.key});
+
+  @override
+  ConsumerState<NewPlaceScreen> createState() => _NewPlaceScreenState();
+}
+
+class _NewPlaceScreenState extends ConsumerState<NewPlaceScreen> {
+  final _titleController = TextEditingController();
+  File? _selectedImage;
+  void _savePlace() {
+    final enteredText = _titleController.text;
+    if (enteredText.isEmpty || _selectedImage == null) {
+      return;
+    }
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredText, _selectedImage!);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add new Place'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              style:
+                  TextStyle(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            //Image Input
+            const SizedBox(
+              height: 16,
+            ),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            LocationInput(),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton.icon(
+                icon: Icon(Icons.add),
+                onPressed: _savePlace,
+                label: const Text('Add Place'))
+          ],
+        ),
+      ),
+    );
+  }
+}
