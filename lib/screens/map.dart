ac974099100cc6a1f07ec0e5a20ee497781115a8
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-03-03 14:31:25 +0700

    Handling Map Taps for Selecting a Location Manually

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -15,6 +15,7 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,16 +28,22 @@ class _MapScreenState extends State<MapScreen> {
           ],
         ),
         body: GoogleMap(
+          onTap: (position) {
+            _pickedLocation = position;
+          },
           initialCameraPosition: CameraPosition(
             target: LatLng(widget.location.latitude, widget.location.longitude),
             zoom: 16,
           ),
-          markers: {
-            Marker(
-                markerId: const MarkerId('m1'),
-                position:
-                    LatLng(widget.location.latitude, widget.location.longitude))
-          },
+          markers: (_pickedLocation == null && widget.isSelecting == true)
+              ? {}
+              : {
+                  Marker(
+                      markerId: const MarkerId('m1'),
+                      position: _pickedLocation ??
+                          LatLng(widget.location.latitude,
+                              widget.location.longitude))
+                },
         ));
   }
 }
